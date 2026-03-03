9c43fdcb8e435bc9caf30abe984bb283cc8a59b5
dsuvan2009-droid <dsuvan2009@gmail.com>
2026-03-03 20:13:57 +0800

    secondcommit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ a=int(input("Enter a number: "))
 b=int(input("Enter a number: "))
 c=int(input("Enter a number: "))
 if a>b and a>c:
-    print("The largest number is: ",a)
+    print("The suitable temperature for wearing light clothes is: ",a)
 elif b>a and b>c:
-    print("The largest number is: ",b)
+    print("The suitable temperature for wearing light clothes is: ",b)
 else:
-    print("The largest number is: ",c)
+    print("The suitable temperature for wearing light clothes is: ",c)
