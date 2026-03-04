1a40df13df9905aceadfa0a3fd2fc6e00b8aaca4
dsuvan2009-droid <dsuvan2009@gmail.com>
2026-03-04 22:22:10 +0800

    secondcommit

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ c=int(input("Enter a number: "))
 if a>b and a>c:
     print("The suitable temperature for wearing light clothes is: ",a)
 elif b>a and b>c:
-    print("The suitable temperature for wearing light clothes is: ",b)
+    print("The suitable temperature for wearing thick clothes is: ",b)
 else:
-    print("The suitable temperature for wearing light clothes is: ",c)
+    print("The suitable temperature for wearing warm clothes is: ",c)
